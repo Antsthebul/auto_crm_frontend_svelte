1549e763ab5f689134f1c87b409c546430973ceb
antsthebul <anthony.allen.srt@gmail.com>
2025-01-27 23:26:20 -0500

    Start touchless

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,39 @@
+<style>
+    #touchless{
+        height: 2em;
+        width: 300px;
+        border: 1px solid lightgrey;
+        border-radius: 15px;
+        margin: 0 auto;
+        padding:5px;
+    }
+    
+    #filterContainer{
+        display: flex;
+        gap:5px;
+    }
+</style>
 <script lang="ts">
     import { enhance } from "$app/forms";
 
-    let customerInfo:string;
-    
+    const SEARCH_FILTERS = ["tech ID", "tech name", "Appt", "RO", "Customer", "plate"]
+
+    let searchText:string
+
+    $: console.log("Input => ", searchText)
+
+    function wrappedFilter(text:string):string{
+        return `<span style="display: inline-block;
+                    background-color: rgb(30, 208, 208);
+                    color: rgb(255, 255, 255);
+                    text-decoration: solid;
+                    font-size: 1.4em;
+                    padding:5px;">${text}:<span`
+    }
 </script>
+
+<h1>Hello Person</h1>
+
 
 
 <form method="post" action="/api/customers" use:enhance={({ formElement, formData, action, cancel, submitter })=>{
@@ -11,16 +41,14 @@
         formElement.reset()
     }
 }}>
-    <label> Plate
-        <input />
-    </label>
-    <label for="">Name</label>
-    <input type="text" name="name"/>
+    <div id="filterContainer">
 
-    <label for="">Address</label>
-    <input type="text" name="address"/>
-
-    <button class="btn btn-info"> Submit</button>
+        {#each SEARCH_FILTERS as search}
+        <span class="selectable">{search}</span>
+        {/each}
+    </div>
+     <div id="touchless" contenteditable bind:innerHTML={searchText}></div>
 </form>
 <h3>Appointments</h3>
 <h3>Repair Orders</h3>
+<h3>Techs</h3>
